c5730a43cc01586fc7a5570452ec42871946e5f3
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-05-20 22:08:53 +0300

    Registered catalog models on the administration site

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        ordering = ("name",)
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
-        def __str__(self):
-            return self.name
-        
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
-    Category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    Category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -25,8 +29,8 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('name',)
-        index_together = (('id', 'slug'),)
-    
-        def __str__(self):
-            return self.name
+        index_together = (("id", "slug"),)
 
+    def __str__(self):
+        return self.name
+    
